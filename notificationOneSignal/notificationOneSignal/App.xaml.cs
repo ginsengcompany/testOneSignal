4de555c3eb34e1eda663b3c036166db33b95dff5
aldo <aldo.minasi@gmail.com>
2017-12-11 14:46:55 +0100

    prova

---
+++ notificationOneSignal/notificationOneSignal/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Com.OneSignal;
+using Xamarin.Forms;
+
+namespace notificationOneSignal
+{
+    public partial class App : Application
+    {
+
+        private string app_id = "247b8425-652f-468e-800f-5a4559bb34c5";
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new notificationOneSignal.MainPage();
+
+            OneSignal.Current.StartInit(app_id).InFocusDisplaying(Com.OneSignal.Abstractions.OSInFocusDisplayOption.Notification).EndInit();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
